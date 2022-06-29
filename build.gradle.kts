d7263e13249f6bee855e59092b09d1cf18048f39
Maxim <maksukh@inbox.ru>
2022-06-29 17:00:49 +0300

    Fix build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,23 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.21"
+    application
 }
 
 group = "ru.chimchima"
 version = "1.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+application {
+    mainClass.set("ru.chimchima.ChimberBotKt")
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("dev.kord:kord-core:0.8.0-M14")
     implementation("org.slf4j:slf4j-simple:1.7.36")
 }
@@ -23,4 +29,12 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "17"
         languageVersion = "1.6"
     }
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "ru.chimchima.ChimberBotKt"
+    }
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
