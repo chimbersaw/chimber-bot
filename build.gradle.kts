0b0864a61da17ddfa823e3196341631133bce1e6
Maxim Sukhodolskii <maksukh@inbox.ru>
2023-06-19 19:12:23 +0300

    Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.serialization") version "1.8.21"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
     application
 }
 
@@ -22,14 +22,14 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
     implementation("dev.kord:kord-core:0.9.0")
     implementation("dev.kord:kord-core-voice:0.9.0")
 
-    implementation("com.github.walkyst:lavaplayer-fork:1.4.0")
-    implementation("io.ktor:ktor-client-core:2.3.0")
+    implementation("com.github.walkyst:lavaplayer-fork:1.4.2")
+    implementation("io.ktor:ktor-client-core:2.3.1")
     implementation("org.slf4j:slf4j-simple:2.0.7")
 }
 
