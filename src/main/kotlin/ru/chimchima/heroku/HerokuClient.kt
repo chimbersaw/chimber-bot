cd275d338096c64b024dfe6458852d25d9d978ee
Maksim Sukhodolskii <maksukh@inbox.ru>
2023-12-25 11:13:22 +0300

    MC RUslan on the beat

--- src/main/kotlin/ru/chimchima/heroku/HerokuClient.kt
+++ src/main/kotlin/ru/chimchima/heroku/HerokuClient.kt
@@ -6,15 +6,15 @@ import io.ktor.http.*
 import ru.chimchima.utils.LocalProperties
 import ru.chimchima.utils.performRequest
 
-private const val HEROKU_CHIMBER_BOT_APP = "https://api.heroku.com/apps/chimber-bot/dynos"
+private const val HEROKU_CHIMBER_BOT_DYNOS = "https://api.heroku.com/apps/chimber-bot/dynos"
 
 class HerokuClient {
     private val client = HttpClient()
-    private val token = LocalProperties.herokuToken
+    private val token = LocalProperties.herokuToken.orEmpty()
 
     suspend fun restart(): Boolean {
-        val response = client.delete(HEROKU_CHIMBER_BOT_APP) {
-            bearerAuth(token ?: "")
+        val response = client.delete(HEROKU_CHIMBER_BOT_DYNOS) {
+            bearerAuth(token)
             accept(ContentType("application", "vnd.heroku+json; version=3"))
         }
 
